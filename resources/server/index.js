c8ceba440f06a8f7a7ec9bee7ea9baa261a5df28
Djumic Branislav <djumicbranislav@outlook.com>
2019-06-11 23:47:47 +0200

    Added 404 route View engine set to Handlebars | Update #7 |

--- resources/server/index.js
+++ resources/server/index.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const bodyParser = require("body-parser");
+const handlebars = require("handlebars");
 const http = require("http");
 const socketIO = require("./routes/socketio");
 const express = require("express");
@@ -8,11 +9,17 @@ const app = express(),
   server = http.createServer(app),
   io = socketIO.listen(server);
 
-app.get("/", (req, res) => {
-  res.render(path.join(__dirname, "..", "html", "index.html"));
-});
+app.set("view engine", "handlebars");
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "..")));
+app.use(express.static(path.join(__dirname, "..", "..", "dist")));
+
+app.get("/", (req, res) => {
+  res.render(path.join(__dirname, "..", "..", "dist", "index"));
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("404 - File not found");
+});
 
 server.listen(port, () => console.log(`Server listening on port ${port}`));
