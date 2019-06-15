d2d0f531e380eac4c82d379a979ed9ab7624be3e
Djumic Branislav <djumicbranislav@outlook.com>
2019-06-15 13:46:55 +0200

    Logic moved to app.js | Update #8 |

--- resources/server/index.js
+++ resources/server/index.js
@@ -1,25 +1,11 @@
-const path = require("path");
-const bodyParser = require("body-parser");
-const handlebars = require("handlebars");
 const http = require("http");
+
+const app = require("./app");
 const socketIO = require("./routes/socketio");
-const express = require("express");
+
 const port = process.env.PORT || 3000;
-const app = express(),
-  server = http.createServer(app),
-  io = socketIO.listen(server);
 
-app.set("view engine", "handlebars");
-
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "..", "..", "dist")));
-
-app.get("/", (req, res) => {
-  res.render(path.join(__dirname, "..", "..", "dist", "index"));
-});
-
-app.get("*", (req, res) => {
-  res.status(404).send("404 - File not found");
-});
+const server = http.createServer(app);
+const io = socketIO.listen(server);
 
 server.listen(port, () => console.log(`Server listening on port ${port}`));
