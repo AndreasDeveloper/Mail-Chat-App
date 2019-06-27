69588a0a56c0460ee12a30c1b6351e1f62f0c185
Lord of Destiny <djumicbranisav@outlook.com>
2019-06-27 11:34:19 +0200

    Auth middleware protects routes allowed only for logged in users | Update #9 |

---
+++ resources/server/middleware/auth.js
@@ -0,0 +1,13 @@
+module.exports = () => {
+  return (req, res, next) => {
+    console.log(
+      `req.session.passport.user: ${JSON.stringify(req.session.passport)}`
+    );
+
+    if (req.isAuthenticated()) {
+      return next();
+    }
+
+    res.redirect("/login");
+  };
+};
